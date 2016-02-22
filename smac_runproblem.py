c96e8c1cc0cb194b63822f4084ee019c97ea3df5
Ruben Burger <Ruben.burger@gmail.com>
2016-02-22 10:58:30 +0100

    small changes + scene/state load now automatic

--- smac_runproblem.py
+++ smac_runproblem.py
@@ -24,10 +24,14 @@ from datetime import datetime as dt
 
 logging.basicConfig(level=logging.INFO)
 									
-def bm_run_cost(planner_type,configMap):
+def bm_run_cost(problem_config, configMap):
 	""" benchmark run that sets parameters and generates a cost  """
 
 	planner_name = "SMAC"
+
+	planner_type = problem_config[0]
+	scene_number = problem_config[1]
+	problem_iterations = int(problem_config[2])
 
 	rospy.set_param("/move_group/planner_configs/" + planner_name + "/type",
 		planner_type)
@@ -37,41 +41,46 @@ def bm_run_cost(planner_type,configMap):
 	robot = moveit_commander.RobotCommander()
 	rospy.sleep(0.2)
 
-	planning_frame = group.get_planning_frame() 
+	planning_frame = group.get_planning_frame()
 
 	group.set_planning_time(3)
 	group.set_planner_id(planner_name)
 	
 	base_string = "/move_group/planner_configs/"+ planner_name +"/"
 	for name, value in configMap.items():
-		rospy.set_param(base_string+name,float(value))				
-	
-	states = [[1.2119047900726956, -1.0789121808903783, 1.7089048559038458,
-	 		-1.2580455469851477, -0.5853913290905349, 2.1585251655353033], 
-	 		[-1.0809913028897857, -1.2672336893382499, -0.8308119476540057,
-	 		 -1.8211554831620005, 2.6605030443567, -2.244767596407182],
-	 		[-0.3213221270871598, -0.7064710071721609, 1.16905227652397,
-	 		 -0.8275752604135796, -1.9994209399472982, 1.4569111290179193], 
-	 		[-1.6430097177151217, -1.7009085861670843, -1.9497761699343577, 
-	 		 2.7055998875277325, 0.11668101664456862, 2.2915025755006893], 
-	 		[-2.828711205610591, -2.884992362653886, -0.5500342441045457,
-	 		 -0.6611208594724118, 2.404477588566261, -0.8306490600328073]]
+		rospy.set_param(base_string+name,float(value))
+
+	with open("files/scene_" + scene_number + ".states") as f:
+		lines = 0   # get lines to know amount of states
+		for line in f:
+			lines += 1
+		f.seek(0)   #reset readline
+		
+		states = [[0 for x in xrange(6)] for x in xrange(lines)] # 6 by Lines list for storing the states
+		
+		for num in range (0, lines):
+			states[num] = eval(f.readline())
+
+
+
 
 	result = 0		   		            
-	for x2 in xrange(5):
+	for x2 in xrange(len(states)):
 		start = states[x2]
-		for x3 in range (x2+1, 5):         #loops to decide start and goal states
+		for x3 in xrange(len(states)):         #loops to decide start and goal states
+			if (x2==x3): break              #break if start/goal is same
 			goal = states[x3]                            										
 			query = {"start":start, "goal":goal}        #create query dict										
-			for it in range (0, 1):						
+			for it in range (0, problem_iterations):						
 				start_state = robot.get_current_state()
 				start_state.joint_state.position = start				
 				group.set_start_state(start_state)
 				group.set_joint_value_target(goal)     #takes a list as input
 				start_time = time.time()                # *1000 to get millisecond
 				planned_path = group.plan()
-				result += (time.time()-start_time)					
+				result += (time.time()-start_time)
 	return(result)
+
 
 if __name__ == "__main__":
 	""" The main, init roscpp and rospy, construct class and launch benchmark """
@@ -81,18 +90,22 @@ if __name__ == "__main__":
 
 	# Read in first arguments.
 	planner_type = sys.argv[1]
+	scene_number = sys.argv[2]
+	problem_iterations = sys.argv[3]
 
-	instance = sys.argv[2]
-	specifics = sys.argv[3]
-	cutoff = int(float(sys.argv[4]) + 1)
-	runlength = int(sys.argv[5])
-	seed = int(sys.argv[6])
+	problem_config = [sys.argv[1],sys.argv[2],sys.argv[3]]
+
+	instance = sys.argv[4]
+	specifics = sys.argv[5]
+	cutoff = int(float(sys.argv[6]) + 1)
+	runlength = int(sys.argv[7])
+	seed = int(sys.argv[8])
 	 
 	# Read in parameter setting and build a dictionary mapping param_name to param_value.
-	params = sys.argv[7:]
+	params = sys.argv[9:]
 	configMap = dict((name[1:], value) for name, value in zip(params[::2], params[1::2]))
 
-	quality = bm_run_cost(planner_type, configMap)
+	quality = bm_run_cost(problem_config, configMap)
 
 	print("Result for SMAC: SUCCESS, " + str(quality) + ", 0, 0, 0")
 
--- smacit.py
+++ smacit.py
@@ -10,8 +10,14 @@ import os
 
 planner_type = "geometric::RRTConnect"
 pcs_file = "RRTC.pcs"
+scene_number = "4"
+problem_iterations = "5"
 smac_iterations = 100
 
+
+##############################################################
+
+os.system("python load_scene.py " + scene_number)
 name = "tmpscenario.txt"
 try:
 	tfile = open(name,'a') 
@@ -20,11 +26,15 @@ try:
 	tfile.write("runObj = RUNTIME\n")
 	tfile.write("deterministic = 1\n")
 	tfile.write("pcs-file = pcs/" + pcs_file + "\n")
-	tfile.write("algo = python smac_runproblem.py " + planner_type + "\n")
+	tfile.write("algo = python smac_runproblem.py " + planner_type + " " + scene_number + " " + problem_iterations + "\n")
 	tfile.close()
 except:
 	print('Something went wrong!')
 	sys.exit(0)
 
 os.system("smac/smac --scenario-file " + name)
+
 os.system("rm " + name)
+os.system("python clear_scene.py " + scene_number)
+
+###############################################################
